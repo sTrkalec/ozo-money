bc59a092d78a879ad6fd6b2a91645577703e6b74
Gabriel Miranda <vytorgabriel123v@gmail.com>
2023-02-18 11:59:08 -0300

    feat: cria e termina css e html do modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 import LogoImg from "../../assets/logo.svg"
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionsModal } from "../NewTransactionsModal";
 
 export function Header() {
     return (
@@ -8,8 +10,13 @@ export function Header() {
             <HeaderContent>
                 <img src={LogoImg} alt="" />
 
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
 
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+                    <NewTransactionsModal/>
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
